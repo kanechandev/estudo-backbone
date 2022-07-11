89e02dc02b59f8c0d16c1f875ae9233ebbf2eaab
cursoskanechan <kanes.chan@gmail.com>
2022-07-11 10:16:10 -0300

    feat: working with attributes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 var Song = Backbone.Model.extend({
-    initialize: function(){
-        console.log("A new song has been created.");
+    defaults: {
+        genre: "Jazz"
     }
 });
 
 var song = new Song();
+song.set("title", "Blue in Green");
+song.set({
+    artist: "Miles Davis",
+    publishYear: 1959
+});
+
+/*
+    Working with Attributes
+
+    song.set("title", "Blue in Green");
+
+    var title = song.get("title");
+
+    var hasTitle = song.has("title");
+
+    song.unset("title");
+
+    song.clear();
+*/
