0fb3e33c227ff63aacdcd7ab2137429962eb167f
cursoskanechan <kanes.chan@gmail.com>
2022-07-11 16:41:40 -0300

    feat: Creating Views.

--- js/main.js
+++ js/main.js
@@ -1,46 +1,20 @@
-		
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var SongView = Backbone.View.extend({
+    tagName: "span",
+    className: "song",
+    id: "1234",
 
-var Vehicle = Backbone.Model.extend({
+    attributes: {
+        "data-genre": "Jazz"
+    },
 
-	idAttribute: "registrationNumber",
+    render: function(){
+        this.$el.html("Hello World");
 
-	urlRoot: "/api/vehicles",
-
-	validate: function(attrs){
-		if (!attrs.registrationNumber)
-			return "Vehicle is not valid.";
-	},
-
-	start: function(){
-		console.log("Vehicle started.");
-	}
+        return this;
+    }
 });
 
-var Vehicles = Backbone.Collection.extend({
-	Model: Vehicle
-});
+var songView = new SongView();
+//songView.render();
 
-var Car = Vehicle.extend({
-	start: function(){
-		console.log("Car with registration number " + this.get("registrationNumber") + " started.");
-	}
-});
-
-var vehicles = new Vehicles([
-	new Car({ registrationNumber: "XLI887", color: "Blue" }),
-	new Car({ registrationNumber: "ZNP123", color: "Blue" }),
-	new Car({ registrationNumber: "XUV456", color: "Gray" })
-]);
-
-var blueCars = vehicles.where({ color: "Blue" });
-console.log("Blue cars", blueCars);
-
-var carXLI887 = vehicles.findWhere({ registrationNumber: "XLI887" });
-console.log("Car with registration number XLI887", carXLI887);
-
-vehicles.remove(carXLI887);
-
-console.log("Vehicles as JSON", vehicles.toJSON());
+$("#container").html(songView.render().$el);
